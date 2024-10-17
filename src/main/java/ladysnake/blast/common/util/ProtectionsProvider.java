c60ad134c5cef67bf98b5db39de997692d7d0962
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-10-17 14:18:22 -0500

    util cleanup

--- src/main/java/ladysnake/blast/common/util/ProtectionsProvider.java
+++ src/main/java/ladysnake/blast/common/util/ProtectionsProvider.java
@@ -19,43 +19,46 @@ public class ProtectionsProvider {
 
     public static boolean canDamageEntity(Entity entity, DamageSource damageSource) {
         return checkProtection(entity.getWorld(), entity.getBlockPos(), damageSource, ret ->
-                CommonProtection.canDamageEntity(entity.getWorld(), entity, ret.getFirst(), ret.getSecond()));
+            CommonProtection.canDamageEntity(entity.getWorld(), entity, ret.getFirst(), ret.getSecond()));
     }
 
     public static boolean canInteractEntity(Entity entity, DamageSource damageSource) {
         return checkProtection(entity.getWorld(), entity.getBlockPos(), damageSource, ret ->
-                CommonProtection.canInteractEntity(entity.getWorld(), entity, ret.getFirst(), ret.getSecond()));
+            CommonProtection.canInteractEntity(entity.getWorld(), entity, ret.getFirst(), ret.getSecond()));
     }
 
     public static boolean canExplodeBlock(BlockPos blockPos, World world, Explosion explosion, DamageSource damageSource) {
         return checkProtection(world, blockPos, damageSource, ret ->
-                CommonProtection.canExplodeBlock(world, blockPos, explosion, ret.getFirst(), ret.getSecond()));
+            CommonProtection.canExplodeBlock(world, blockPos, explosion, ret.getFirst(), ret.getSecond()));
     }
 
     public static boolean canPlaceBlock(BlockPos blockPos, World world, DamageSource damageSource) {
         return checkProtection(world, blockPos, damageSource, ret ->
-                CommonProtection.canPlaceBlock(world, blockPos, ret.getFirst(), ret.getSecond()));
+            CommonProtection.canPlaceBlock(world, blockPos, ret.getFirst(), ret.getSecond()));
     }
 
     public static boolean canPlaceBlock(BlockPos blockPos, World world, @Nullable PlayerEntity player) {
         var gameProfile = (player != null) ? player.getGameProfile() : null;
-        if (gameProfile != null)
-            return CommonProtection.canPlaceBlock(world, blockPos, gameProfile,player);
-        else
+        if (gameProfile != null) {
+            return CommonProtection.canPlaceBlock(world, blockPos, gameProfile, player);
+        } else {
             return !CommonProtection.isProtected(world, blockPos);
+        }
     }
 
     private static boolean checkProtection(World world, BlockPos blockPos, DamageSource damageSource, Predicate<Pair<GameProfile, ServerPlayerEntity>> protectionPredicate) {
         Entity attacker = null;
 
-        if (damageSource.getAttacker() != null)
+        if (damageSource.getAttacker() != null) {
             attacker = damageSource.getAttacker();
-        else if (damageSource.getSource() instanceof BombEntity bombEntity)
+        } else if (damageSource.getSource() instanceof BombEntity bombEntity) {
             attacker = bombEntity.getOwner();
+        }
 
-        if (attacker instanceof ServerPlayerEntity playerAttacker)
+        if (attacker instanceof ServerPlayerEntity playerAttacker) {
             return protectionPredicate.test(Pair.of(playerAttacker.getGameProfile(), playerAttacker));
-        else
+        } else {
             return !CommonProtection.isProtected(world, blockPos);
+        }
     }
 }
