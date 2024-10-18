7eec554ccd1859e9776a40c37b7d3d95cc7ad66d
MoriyaShiine <45156123+MoriyaShiine@users.noreply.github.com>
2024-10-18 10:18:37 -0500

    pipe bombs in bundles are now hidden

--- src/main/java/ladysnake/blast/mixin/BundleItemMixin.java
+++ src/main/java/ladysnake/blast/mixin/BundleItemMixin.java
@@ -1,15 +1,23 @@
 package ladysnake.blast.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 import ladysnake.blast.common.init.BlastComponentTypes;
 import ladysnake.blast.common.init.BlastItems;
+import net.minecraft.component.ComponentType;
+import net.minecraft.component.type.BundleContentsComponent;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.StackReference;
 import net.minecraft.item.BundleItem;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ClickType;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.math.random.Random;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -28,11 +36,40 @@ public class BundleItemMixin {
         prime(otherStack, player);
     }
 
+    @WrapOperation(method = "getTooltipData", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;get(Lnet/minecraft/component/ComponentType;)Ljava/lang/Object;"))
+    private Object blast$showFakeItem(ItemStack instance, ComponentType<BundleContentsComponent> componentType, Operation<Object> original) {
+        Object component = original.call(instance, componentType);
+        if (component instanceof BundleContentsComponent bundleContentsComponent) {
+            BundleContentsComponent.Builder builder = new BundleContentsComponent.Builder(BundleContentsComponent.DEFAULT);
+            for (int i = 0; i < bundleContentsComponent.size(); i++) {
+                ItemStack stack = bundleContentsComponent.get(i);
+                if (stack.contains(BlastComponentTypes.FAKE_ITEM_ID)) {
+                    builder.add(new ItemStack(Registries.ITEM.get(stack.get(BlastComponentTypes.FAKE_ITEM_ID)), stack.getCount() * (64 / stack.getMaxCount())));
+                } else {
+                    builder.add(stack);
+                }
+            }
+            return builder.build();
+        }
+        return component;
+    }
+
     @Unique
     private static void prime(ItemStack stack, PlayerEntity player) {
         if (stack.isOf(BlastItems.PIPE_BOMB)) {
             player.playSound(SoundEvents.BLOCK_TRIPWIRE_CLICK_ON, 0.5F, 1);
             stack.set(BlastComponentTypes.PRIMED, true);
+            stack.set(BlastComponentTypes.FAKE_ITEM_ID, getRandomFakeItem(player.getRandom()));
         }
+    }
+
+    @Unique
+    private static Identifier getRandomFakeItem(Random random) {
+        Item item;
+        do {
+            item = Registries.ITEM.get(random.nextInt(Registries.ITEM.size()));
+        }
+        while (item.getMaxCount() != 64);
+        return Registries.ITEM.getId(item);
     }
 }
